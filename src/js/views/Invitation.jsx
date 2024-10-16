f4ef912805c2b043cf09c49c34a8251109ceb0ae
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2024-10-16 22:37:32 +0200

    gif

--- src/js/views/Invitation.jsx
+++ src/js/views/Invitation.jsx
@@ -4,28 +4,18 @@ import prueba1 from "../../img/prueba1.jpg";
 import logo from "../../img/logo1.png";
 import herculesscene from "../../img/herculesscene.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocation, faClock } from "@fortawesome/free-solid-svg-icons";
+import { faLocation, faClock, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 
 export const Invitation = () => {
-    const [selectedOption, setSelectedOption] = useState("");
-    const [isFormVisible, setIsFormVisible] = useState(false);
+    // Crear constante de cambio de estado useState
+    const [selectedoption, setSelectedoption] = useState("");
 
-    const handleOptionChange = (event) => {
-        const value = event.target.value;
-        setSelectedOption(value);
-        if (value === "1") {
-            setIsFormVisible(true);
-        } else {
-            setIsFormVisible(false);
-        }
+    // Para cambiar la opción
+    const handleSelectChange = (e) => {
+        setSelectedoption(e.target.value);
     };
 
-    const handleFormClick = (event) => {
-        if (!isFormVisible) {
-            event.preventDefault(); // Evitar que el enlace sea seguido si el formulario no está disponible
-        }
-    };
 
     return (
         <div className="bigone container-fluid d-flex flex-column align-items-center justify-content-center p-2 m-2">
@@ -43,23 +33,27 @@ export const Invitation = () => {
                                     ¡Por fin! El evento más esperado se acerca y si has recibido este enlace, es porque eres una persona importante para las dos y queremos que
                                     disfrutes de este día a nuestro lado.
                                 </p>
-                                <div className="d-flex justify-content-center p-1 m-auto">
-                                    <select className="form-select w-auto" value={selectedOption} onChange={handleOptionChange} aria-label="Default select example">
-                                        <option value="" disabled hidden>¿Vienes?</option>
-                                        <option value="1">Cuenta con mi hacha</option>
-                                        <option value="2">Todavía no lo sé</option>                                        <option value="3">No puedo :( </option>
+                                <div className="d-flex justify-content-center">
+                                    {/* <div className="dropdown">
+                                        <button className="btn btn-outline-secondary dropdown-toggle m-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            ¿Vienes?
+                                        </button>
+                                        <ul className="dropdown-menu">
+                                            <li><a className="dropdown-item ok">Cuenta con mi hacha</a></li>
+                                            <li><a className="dropdown-item ns">Todavía no lo sé</a></li>
+                                            <li><a className="dropdown-item ko">No... </a></li>
+                                        </ul>
+                                    </div> */}
+                                    <select className="form-select" aria-label="Default select example" value={selectedoption} onChange={handleSelectChange}>
+                                        <option selected>¿Vienes?</option>
+                                        <option value="option1">Cuenta con mi hacha</option>
+                                        <option value="option2">Todavía no lo sé</option>
+                                        <option value="option3">No puedo...</option>
                                     </select>
-                                </div>
-                                {isFormVisible && (
+                                    <img src={herculesscene} className="w-25 p-1" alt="hercules" />
                                     <div>
-                                    <a href="https://forms.gle/KoKZS3pXpu4uzBvb8" className="btn btn boton1 p-2 m-1 disabled" onClick={handleFormClick}>
-                                        Formulario
-                                    </a>
-                                    <div className="alert alert-danger mt-2" role="alert">
-                                        El formulario todavía no está disponible. 
                                     </div>
-                                    </div>
-                                    )}
+                                </div>
                             </div>
                             {/* Aquí lo que muestra por cada opción que se elija */}
                             {selectedoption === "option1" && (
@@ -91,7 +85,7 @@ export const Invitation = () => {
                         </ul>
                     </div>
                     <br />
-                    <div className="mt-2 d-flex flex-column align-items-center">
+                    <div className="mt-3 d-flex flex-column align-items-center">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3509.4213625752946!2d-16.533334924841036!3d28.406539975789002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc41d559de7ae8a5%3A0x9d97500c7f545aed!2sLa%20Ga%C3%B1an%C3%ADa%20Finca%20%26%20Catering!5e0!3m2!1ses!2ses!4v1721477008483!5m2!1ses!2ses"
                             width="300"
@@ -100,19 +94,15 @@ export const Invitation = () => {
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
-                            target="_blank"
-                            rel="noopener noreferrer"
                         ></iframe>
                     </div>
                     <div className="p-3">
                         <h4>Empieza a ensayar que vienen curvas</h4>
-                        <a href="https://open.spotify.com/playlist/1P6qv6TtNvUvHEvrvpkoPK?si=-pU9mgDaRUmZPJGwUmjrMg&pt=17e02215c918a83516b63cfccaf0191" className="btn btn-success boton2 p-1">
-                            <FontAwesomeIcon icon={faSpotify} /> Spotify
-                        </a>
+                        <a href="https://open.spotify.com/playlist/1P6qv6TtNvUvHEvrvpkoPK?si=-pU9mgDaRUmZPJGwUmjrMg&pt=17e02215c918a83516b63cfccaf0191" className="btn btn-success boton2 p-1"><FontAwesomeIcon icon={faSpotify} /> Spotify </a>
                     </div>
                     <div className="p-3">
                         <p>Sí, tu presencia es suficiente ya lo sabes, pero oye, si te apetece contribuir y tal...
-                            Nos mola el rollito efectivo bajo el colchón, pero si no es viable, puede dejar su contribución aquí:
+                            Nos mola el rollito efectivo bajo el colchón, pero si no es viable, habla con nosotras.
                             <br />
                         </p>
                     </div>
